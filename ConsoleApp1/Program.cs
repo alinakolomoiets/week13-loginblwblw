5066d328e2ae87ab71d70f4e9309eea93c14a275
Opilane TTHK <opilane@tthk.ee>
2021-11-23 10:04:02 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // login:admin
+            //password:pass
+
+            //declare veriables
+            //assign value 
+            string login = "admin";
+            string password = "pass1234";
+
+            string userName, userPasword;
+            Console.WriteLine("Enter yout login:");
+            userName = Console.ReadLine();
+            Console.WriteLine("Enter your password:");
+            userPasword = Console.ReadLine();
+
+            if(login== userName&& password ==userPasword)
+            {
+                Console.WriteLine("Welccome");
+            }
+            else
+            {
+                Console.WriteLine("Oops. Something went wrong.");
+            }
+        }
+
+    }
